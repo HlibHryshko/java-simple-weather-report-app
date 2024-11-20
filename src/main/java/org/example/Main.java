3b0dd18e489a437cd6a5f5b3d7735cfd61fd9411
Hlib Hryshko <hlib@siip.group>
2024-11-20 18:10:56 +0100

    reading from file line by line is implemented

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,30 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // new WeatherReportCore(new WeatherReportFileReader(new WeatherReportFileParser))
+        // sout WeatherReportCore.hottestDay()
+        // sout WeatherReportCore.coldestDay()
+
+        try(
+            BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                    Objects.requireNonNull(
+                            Main.class.getClassLoader().getResourceAsStream("input.txt")
+                    )
+                )
+            )
+        ) {
+            String line;
+            while( (line = reader.readLine()) != null ) {
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
